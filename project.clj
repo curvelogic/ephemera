86ee5b86f65c19fcfbeaea4e61df99ff546ebc05
gmorpheme <github@gmorpheme.net>
2015-02-12 17:41:13 +0000

    Add lein-midje plugin.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
 
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
